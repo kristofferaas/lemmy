3b874fd8ec96fbee2d61ed5887b354da80d06284
Kristoffer Aas <kristoffer.aas@fortedigital.com>
2023-08-16 15:20:04 +0200

    Creates app shell components

--- components/app/app-provider.tsx
+++ components/app/app-provider.tsx
@@ -3,12 +3,16 @@
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState } from "react";
+import { ThemeProvider } from "../ui/theme";
+import { Provider as TextBalancerProvider } from "react-wrap-balancer";
 
-export function QueryProvider({ children }: { children: React.ReactNode }) {
+export function AppProvider({ children }: { children: React.ReactNode }) {
   const [client] = useState(() => new QueryClient());
   return (
     <QueryClientProvider client={client}>
-      {children}
+      <ThemeProvider attribute="class" defaultTheme="system">
+        <TextBalancerProvider>{children}</TextBalancerProvider>
+      </ThemeProvider>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
