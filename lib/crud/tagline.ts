ed8ff62c727a1d8a64297d3b973797d4210b8bed
Kristoffer Aas <kristoffer.aas@fortedigital.com>
2023-08-30 10:49:14 +0200

    Adds tagline transformer

--- lib/crud/tagline.ts
+++ lib/crud/tagline.ts
@@ -1,13 +1,20 @@
 import { InferSelectModel, eq } from "drizzle-orm";
 import { db } from "../db";
 import { localSite, tagline } from "../db/schema/schema";
+import { Tagline } from "lemmy-js-client";
+import { replaceNullWithUndefined } from "../utils/replaceNullWithUndefined";
 
 export async function getAllTaglines(
-  localSiteId: InferSelectModel<typeof localSite>["id"]
+  localSiteId: InferSelectModel<typeof localSite>["id"],
 ) {
   const response = await db
     .select()
     .from(tagline)
     .where(eq(localSite.id, localSiteId));
-  return response;
+
+  return fiddle(response);
 }
+
+const fiddle = (items: InferSelectModel<typeof tagline>[]): Tagline[] => {
+  return replaceNullWithUndefined(items);
+};
